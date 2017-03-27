d665e649cac75dc7d3f830fa0e3c683aa113f927
Sujan Shrestha <shrsujan2007@gmail.com>
2017-03-28 02:29:52 +0545

    controller test added, minor changes in others

--- app/chat/controller.js
+++ app/chat/controller.js
@@ -11,46 +11,51 @@ export default Controller.extend({
 
   init() {
     set(this, 'socket', this.get('socketIOService').socketFor('http://localhost:1993/'))
-
-    this.socket.on('userInfo', (user) => {
-      if (user) {
-        set(this, 'user', user)
-        set(this, 'canEnter', true)
-      } else {
-        Ember.$('#errorMsg').show().html('<em>' + this.username + '</em> username is already taken').fadeOut(5000)
-      }
-    })
-
-    this.socket.on('members', (members) => {
-      set(this, 'members', members)
-    })
-
-    this.socket.on('message', (data) => {
-      Ember.$('#chat-box').append('<li class="others-msg pull-left"><b>' + data.User.username.toUpperCase() + ': </b>' + data.message + '</li>')
-      Ember.$('#chat-box').scrollTop(Ember.$('#chat-box')[0].scrollHeight)
-    })
-
-    this.socket.on('allMessages', (messages) => {
-      messages.forEach((message) => {
-        if (this.user.username === message.User.username) {
-          Ember.$('#chat-box').append('<li class="my-msg pull-right"><b>ME: </b>' + message.message + '</li>')
-        } else {
-          Ember.$('#chat-box').append('<li class="others-msg pull-left"><b>' + message.User.username.toUpperCase() + ': </b>' + message.message + '</li>')
-        }
-      })
-      Ember.$('#chat-box').scrollTop(Ember.$('#chat-box')[0].scrollHeight)
-    })
   },
 
   actions: {
     enterChat() {
       this.socket.emit('initialize', this.username)
-      // set(this, 'canEnter', true)
+
+      this.socket.on('userInfo', (user) => {
+        if (user) {
+          set(this, 'user', user)
+          set(this, 'canEnter', true)
+        } else {
+          Ember.$('#errorMsg').show().html('<em>' + this.username + '</em> username is already taken').fadeOut(5000)
+        }
+      })
+
+      this.socket.on('members', (members) => {
+        set(this, 'members', members)
+      })
+
+      this.socket.on('message', (data) => {
+        Ember.$('#chat-box').append('<li class="others-msg pull-left"><b>' + data.User.username.toUpperCase() + ': </b>' + data.message + '</li>')
+        if (Ember.$('#chat-box') && Ember.$('#chat-box').length > 0) {
+          Ember.$('#chat-box').scrollTop(Ember.$('#chat-box')[0].scrollHeight)
+        }
+      })
+
+      this.socket.on('allMessages', (messages) => {
+        messages.forEach((message) => {
+          if (this.user.username === message.User.username) {
+            Ember.$('#chat-box').append('<li class="my-msg pull-right"><b>ME: </b>' + message.message + '</li>')
+          } else {
+            Ember.$('#chat-box').append('<li class="others-msg pull-left"><b>' + message.User.username.toUpperCase() + ': </b>' + message.message + '</li>')
+          }
+        })
+        if (Ember.$('#chat-box') && Ember.$('#chat-box').length > 0) {
+          Ember.$('#chat-box').scrollTop(Ember.$('#chat-box')[0].scrollHeight)
+        }
+      })
     },
 
     sendMessage() {
       Ember.$('#chat-box').append('<li class="my-msg pull-right"><b>ME: </b>' + this.message + '</li>')
-      Ember.$('#chat-box').scrollTop(Ember.$('#chat-box')[0].scrollHeight)
+      if (Ember.$('#chat-box') && Ember.$('#chat-box').length > 0) {
+        Ember.$('#chat-box').scrollTop(Ember.$('#chat-box')[0].scrollHeight)
+      }
       this.socket.emit('message', {
         message: this.message,
         userId: this.user.id
--- tests/unit/chat/controller-test.js
+++ tests/unit/chat/controller-test.js
@@ -1,12 +1,70 @@
 import { moduleFor, test } from 'ember-qunit';
+import Ember from 'ember'
+
+//Stub socket-io service
+const socketIOStub = Ember.Service.extend({
+  socketFor() {
+    return socketFunctions
+  }
+});
+
+const user = {user: {username: 'testUserName', id: 1}}
+
+const socketFunctions = {
+  on(eventName, cb) {
+    switch(eventName) {
+      case 'message':
+        cb({User: {username: 'testUserName'}, message: 'some message'})
+        break;
+      case 'userInfo':
+        cb(user)
+        break;
+      case 'members':
+        cb(['member1', 'member2'])
+        break;
+      case 'allMessages':
+        cb([{User: {username: 'testUserName'}, message: 'some message'}, {User: {username: 'testUserName2'}, message: 'some other message'}])
+        break;
+    default:
+        cb(false)
+        break;
+    }
+  },
+
+  emit(eventName, data) {
+    return data
+  }
+};
 
 moduleFor('controller:chat', 'Unit | Controller | chat', {
   // Specify the other units that are required for this test.
-  // needs: ['controller:foo']
+  // needs: ['service:socket-io']
+  beforeEach: function () {
+    this.register('service:socket-io', socketIOStub);
+    this.inject.service('socket-io', { as: 'socketIOService' });
+  }
 });
 
 // Replace this with your real tests.
 test('it exists', function(assert) {
   let controller = this.subject();
   assert.ok(controller);
+
+  assert.equal(controller.get('canEnter'), false, 'canEnter property checked before entering chat');
+  assert.equal(controller.get('socket'), socketFunctions, 'socket property checked before entering chat');
+  assert.equal(controller.get('user'), null, 'user property checked before entering chat');
+  assert.equal(controller.get('members'), null, 'members property checked before entering chat');
+
+  controller.set('username', 'aUser')
+  controller.set('message', 'something')
+  controller.send('enterChat')
+  
+  assert.equal(controller.get('canEnter'), true, 'canEnter property checked after entering chat');
+  assert.equal(controller.get('username'), 'aUser', 'username property checked after entering chat');
+  assert.deepEqual(controller.get('user'), user, 'user property checked after entering chat');
+  assert.equal(controller.get('message'), 'something', 'message property checked after entering chat');
+
+  controller.send('sendMessage')
+  assert.equal(controller.get('message'), '', 'message property checked after sending message');
+
 });
